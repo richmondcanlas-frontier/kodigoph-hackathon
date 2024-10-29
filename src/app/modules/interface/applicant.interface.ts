2814350a17687ad0f9eb2ffe7dd465af2bcc57dd
Christian M. Tania <christian.tania@frontiersoftwareado.com>
2024-10-29 14:51:21 +0800

    output implementation

---
+++ src/app/modules/interface/applicant.interface.ts
@@ -0,0 +1,14 @@
+export interface Applicant {
+    applicant_id: number;
+    name: string;
+    required_skills_possessed: string[];
+    years_of_work_experience: number;
+    rank: number;
+    total_score: string;
+    skill_score: number;
+    work_experience_score: number;
+    email: string;
+    contact_number: string;
+    address: string;
+    work_experience: string[];
+  }
--- src/app/modules/output/output.component.ts
+++ src/app/modules/output/output.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Applicant } from '../interface/applicant.interface';
 
 @Component({
   selector: 'app-output',
@@ -6,4 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./output.component.scss']
 })
 export class OutputComponent {
+  constructor(private http: HttpClient) { }
+
+  applicantsData: Applicant[] = [];
+
+  ngOnInit() {
+    this.fetchApplicants();
+  }
+    fetchApplicants() {
+      this.http.get('/assets/applicants.json').subscribe((data: any) => {
+       
+        this.applicantsData = data;
+        console.log("data", this.applicantsData);
+      });
+    }
+  
 }
